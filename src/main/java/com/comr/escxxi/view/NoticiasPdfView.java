a305a7469049835dced2735eab693b74ef4bd277
x-omr <x-omr@xomr-li-110-14ISK>
2018-05-08 15:24:35 -0500

    Agregar mas formato a la exportación excel

--- src/main/java/com/comr/escxxi/view/NoticiasPdfView.java
+++ src/main/java/com/comr/escxxi/view/NoticiasPdfView.java
@@ -1,7 +1,7 @@
 package com.comr.escxxi.view;
 
-import static org.assertj.core.api.Assertions.catchThrowable;
 
+import java.awt.Color;
 import java.util.List;
 import java.util.Map;
 
@@ -17,6 +17,7 @@ import com.comr.escxxi.entity.Noticia;
 import com.comr.escxxi.model.ViewNames;
 import com.comr.escxxi.service.NoticiaService;
 import com.lowagie.text.Document;
+import com.lowagie.text.Font;
 import com.lowagie.text.Phrase;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
@@ -33,21 +34,57 @@ public class NoticiasPdfView extends AbstractPdfView{
 	protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer,
 			HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
+		Color colorPrimary = new Color(125, 66, 169);
+		Color colorFontPrimary = new Color(255,255,255);
+		Color colorSecond = new Color(255,134,92);
+		
+		
 		PdfPTable tabla = new PdfPTable(1);	
-		PdfPCell cTitulo = new PdfPCell(new Phrase("NOTICIAS ESC-XXI"));
+		PdfPCell cTitulo = new PdfPCell(new Phrase("NOTICIAS ESC-XXI",new Font(Font.BOLD, 12, Font.ITALIC, colorFontPrimary)));
+		cTitulo.setBackgroundColor(colorPrimary);
+		cTitulo.setPadding(10f);
 		cTitulo.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);			
 		tabla.addCell(cTitulo);
 		
 		
 		PdfPTable nTable = new PdfPTable(4);
-		nTable.addCell("ID");
-		nTable.addCell("Titulo");
-		nTable.addCell("Contenido");
-		nTable.addCell("Fecha");
+		nTable.setWidths(new int[] {8,30,45,17});
+		
+		//ID
+		PdfPCell ctId = new PdfPCell(new Phrase("ID",new Font(Font.BOLD, 12, Font.ITALIC, colorFontPrimary)));
+		ctId.setBackgroundColor(colorSecond);
+		ctId.setPadding(10f);
+		ctId.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+		
+		//Titulo
+		PdfPCell ctTitulo = new PdfPCell(new Phrase("Titulo",new Font(Font.BOLD, 12, Font.ITALIC, colorFontPrimary)));
+		ctTitulo.setBackgroundColor(colorSecond);
+		ctTitulo.setPadding(10f);
+		ctTitulo.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+		
+		//Contenido
+		PdfPCell ctContenido = new PdfPCell(new Phrase("Contenido",new Font(Font.BOLD, 12, Font.ITALIC, colorFontPrimary)));
+		ctContenido.setBackgroundColor(colorSecond);
+		ctContenido.setPadding(10f);
+		ctContenido.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+		
+		
+		//Fecha
+		PdfPCell ctFecha = new PdfPCell(new Phrase("Fecha",new Font(Font.BOLD, 12, Font.ITALIC, colorFontPrimary)));
+		ctFecha.setBackgroundColor(colorSecond);
+		ctFecha.setPadding(10f);
+		ctFecha.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+		
+		nTable.addCell(ctId);
+		nTable.addCell(ctTitulo);
+		nTable.addCell(ctContenido);
+		nTable.addCell(ctFecha);
 		
 		List<Noticia> nList = noticiaService.findByTituloLikeOrContenidoLikeOrderByFechaVigenciaInicio("%");
 		for (Noticia n : nList) {
-			nTable.addCell(n.getNoticiaId().toString());
+			PdfPCell ctIdItem = new PdfPCell(new Phrase(n.getNoticiaId().toString()));
+			ctIdItem.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+			nTable.addCell(ctIdItem);
 			nTable.addCell(n.getTitulo());
 			nTable.addCell(n.getContenido());
 			nTable.addCell(n.getFechaVigenciaInicio().toString());
