5914f960c95c2c188830633c84028ee5a72bc686
x-omr <x-omr@xomr-li-110-14ISK>
2018-07-20 08:18:11 -0500

    imitar rest controller por medio de @ResponseBody con xml y json.

--- src/main/java/com/comr/escxxi/controller/NoticiasController.java
+++ src/main/java/com/comr/escxxi/controller/NoticiasController.java
@@ -1,5 +1,7 @@
 package com.comr.escxxi.controller;
 
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +9,13 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.comr.escxxi.model.NoticiaModel;
 import com.comr.escxxi.model.ViewNames;
 import com.comr.escxxi.service.NoticiaService;
+import com.comr.escxxi.view.xml.NoticiasXmlRoot;
 
 @Controller
 public class NoticiasController {
@@ -27,6 +31,16 @@ public class NoticiasController {
 		LOG.info("/noticias -> " + ViewNames.HomeGeneral.NOTICIAS);
 		ModelAndView mav=new ModelAndView(ViewNames.HomeGeneral.NOTICIAS);						
 		return mav;
+	}
+	
+	//imitar un restcontroller que responda con xml(default) y json segun el deseo del formato que queremos
+	@GetMapping("/noticias-rest")
+	public @ResponseBody NoticiasXmlRoot getNoticiasRest() {
+		LOG.info("/noticias -> noticias-rest");
+		//al pasar el Root de xml matamos 2 pajaros de un tiro ya que no solo responde en xml si no tambine en jsno
+		//para probar hacemos lo siguiente noticias-rest?form=json
+		return new NoticiasXmlRoot(noticiaService.findAllNoticias());
+		
 	}
 	
 	@GetMapping("/noticia/{noticiaId}")
